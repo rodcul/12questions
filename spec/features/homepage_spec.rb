4226ad313478068f4b881c12553f0d24b9e3f69d
Rodney Cullen <rodcul@gmail.com>
2015-06-01 16:47:53 +0100

    Modified spec_helper to get tests working

---
+++ spec/features/homepage_spec.rb
@@ -0,0 +1,7 @@
+require "spec_helper"
+feature 'Show text on homepage' do
+  scenario '12 Question App with bootstrap' do
+    visit '/'
+    expect(page).to have_content('12 Question App with bootstrap')
+  end
+end
