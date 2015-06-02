b479ce70bd714c9dede4b2c3dada4f93e4de644e
Chidumaga <chidumaga@gmail.com>
2015-06-02 14:56:23 +0100

    user can only sign up once

---
+++ spec/features/user/user_signout_spec.rb
@@ -0,0 +1,12 @@
+require_relative '../helpers/sign_up.rb'
+include SignUp
+
+feature 'user signs out' do
+  scenario 'successfully' do
+    sign_up
+    expect(page).to have_content('test@test.com')
+    click_button('Sign out')
+    expect(page).to have_content('Signed out')
+    expect(page).not_to have_content('test@test.com')
+  end
+end
