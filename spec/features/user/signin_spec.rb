1b8ee22402290398bb802ec4f88a4c7983c809a7
Rodney Cullen <rodcul@gmail.com>
2015-06-02 14:40:36 +0100

    Added tests that should pass

--- spec/features/user/signin_spec.rb
+++ spec/features/user/signin_spec.rb
@@ -17,4 +17,21 @@ feature 'user signs in' do
     sign_in('test@test.com', '123')
     expect(page).to have_content('Welcome, test@test.com')
   end
+
+  scenario 'unsuccessfully (unknown email)' do
+    visit '/'
+    expect(page).not_to have_content('test@test.com')
+    click_link('Sign in')
+    sign_in('unkown@test.com', '123')
+    expect(page).to have_content('The email or password is incorrect')
+  end
+
+  scenario 'unsuccessfully (wrong password)' do
+    visit '/'
+    expect(page).not_to have_content('test@test.com')
+    click_link('Sign in')
+    sign_in('test@test.com', 'incorrect')
+    expect(page).to have_content('The email or password is incorrect')
+  end
+
 end
