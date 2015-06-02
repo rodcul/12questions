792ebbb6bf29391715a1a172a04666c7db922084
Rodney Cullen <rodcul@gmail.com>
2015-06-02 15:28:17 +0100

    Renamed spec file

--- spec/features/flash_alerts_spec.rb
+++ spec/features/flash_alerts_spec.rb
@@ -4,10 +4,4 @@ feature 'Show flash alerts' do
     sign_up
     expect(page).to have_content('User signup successful')
   end
-
-  scenario 'error message on signup with existing email' do
-    sign_up
-    sign_up
-    expect(page).to have_content('Email is already taken')
-  end
 end
