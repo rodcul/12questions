7ce348a4b180fc8878d999ce1f2de4286b1488e9
Rocco <roccorighi@hotmail.com>
2015-06-02 12:53:58 +0100

    adds sign in with passing test. no error messages implemented

--- spec/features/user/signup_spec.rb
+++ spec/features/user/signup_spec.rb
@@ -1,9 +1,9 @@
-require_relative '../helpers/sign_up.rb'
-include SignUp
+require_relative '../helpers/session.rb'
+include Session
 
 feature 'User signs up' do
   scenario 'as a new user of the website' do
     expect { sign_up }.to change(User, :count).by(1)
-    expect(page).to have_content('Welcome, test')
+    expect(page).to have_content('Welcome, test@test.com')
   end
 end
