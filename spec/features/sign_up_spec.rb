15c08eaa8d6a4e91ae3774c964b4e192b3c3074d
Rodney Cullen <rodcul@gmail.com>
2015-06-02 11:58:09 +0100

    Fixed test error spotted by Dan

--- spec/features/sign_up_spec.rb
+++ spec/features/sign_up_spec.rb
@@ -2,8 +2,7 @@ require 'spec_helper'
 feature 'Show flash alerts' do
   scenario 'user signup successful' do
     sign_up
-    sign_up
-    expect(page).to have_content('Email is already taken')
+    expect(page).to have_content('User signup successful')
   end
 
   scenario 'error message on signup with existing email' do
